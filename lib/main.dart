20444700bfc48d29be39613c529fd76e28ee2dfd
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-07 19:07:27 -0400

    gerando frases

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -14,6 +15,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var _frase = [
+    "Levanta, sacode a poeira, dá a volta por cima.",
+    "Respire fundo e fique atento às surpresas deliciosas que a vida preparou para você!",
+    "Nem todos os dias são bons, mas há algo bom em cada dia.",
+    "Um passo de cada vez. Até meio passo serve. Siga firme, não há motivo para ter pressa.",
+    "Suba o primeiro degrau com fé. Não é necessário que você veja toda a escada, apenas dê o primeiro passo",
+    "Toda manhã você tem duas escolhas: continuar dormindo com seus sonhos ou acordar e persegui-los!",
+    "A cada novo dia, a cada momento, temos a nossa disposição a maravilhosa possibilidade do encontro, que traz em si infinitas oportunidades. Precisamos apenas estar atentos.",
+    "Dentro de você, há uma luz muito forte! Acredite nela e deixe que ela ilumine o seu dia.",
+    "As pessoas mais felizes não têm as melhores coisas. Elas sabem fazer o melhor das oportunidades que aparecem em seus caminhos.",
+  ];
+
+  var _fraseGerada = "clique abaixo para gerar uma nova frase!";
+
+  void _gerarFrase(){
+
+    var numeroSorteado = Random().nextInt( _frase.length );
+    print(numeroSorteado);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +51,7 @@ class _HomeState extends State<Home> {
           children: [
             Image.asset("image/logo.png"),
             Text(
-              "clique abaixo para gerar uma nova frase!",
+              _fraseGerada,
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 20,
@@ -39,7 +60,10 @@ class _HomeState extends State<Home> {
               ),
             ),
             ElevatedButton(
-              onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+               ),
+              onPressed: _gerarFrase,
                child: Text(
                 "Nova Frase",
                 style: TextStyle(
@@ -48,9 +72,7 @@ class _HomeState extends State<Home> {
                   fontWeight: FontWeight.bold,
                 ),
                ),
-               style: ElevatedButton.styleFrom(
-                 primary: Colors.green,
-               ),
+               
             ),
           ],
         ),

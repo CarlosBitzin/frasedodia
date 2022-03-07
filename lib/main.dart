30c461380bfa3c99c3335b7bb34947daad39cf50
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-07 18:53:20 -0400

    interface criada

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,45 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("image/logo.png"),
+            Text(
+              "clique abaixo para gerar uma nova frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){},
+               child: Text(
+                "Nova Frase",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+               ),
+               style: ElevatedButton.styleFrom(
+                 primary: Colors.green,
+               ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
